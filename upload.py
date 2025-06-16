282f77ef9ff7c5e412007a797629684aedc2685b
denyshorshkov <denys.horshcov@gmail.com>
2025-06-16 17:19:57 +0300

    init new project

--- upload.py
+++ upload.py
@@ -6,10 +6,6 @@ import os
 # --------- 1. PostgreSQL connection config ---------
 load_dotenv()
 
-for key in ['DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_PORT', 'DB_NAME']:
-    val = os.getenv(key)
-    print(f"{key}: {repr(val)}")
-
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
@@ -20,12 +16,16 @@ db_name = os.getenv("DB_NAME")
 engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
 
 # --------- 2. Load CSV files ---------
-stock_prices_df = pd.read_csv("stock_prices.csv")
-company_metrics_df = pd.read_csv("company_metrics.csv")
+stock_prices_df = pd.read_csv("tables/stock_prices.csv")
+rtx_4070_df = pd.read_csv("tables/rtx4070.csv")
+rtx_4080_df = pd.read_csv("tables/rtx4080.csv")
+rtx_4090_df = pd.read_csv("tables/rtx4090.csv")
 
 # --------- 3. Upload to PostgreSQL ---------
 # Replace old table if exists
 stock_prices_df.to_sql("stock_prices", engine, if_exists="replace", index=False)
-company_metrics_df.to_sql("company_metrics", engine, if_exists="replace", index=False)
+rtx_4070_df.to_sql("rtx_4070", engine, if_exists="replace", index=False)
+rtx_4080_df.to_sql("rtx_4080", engine, if_exists="replace", index=False)
+rtx_4090_df.to_sql("rtx_4090", engine, if_exists="replace", index=False)
 
 print("✅ Data successfully uploaded to PostgreSQL.")
