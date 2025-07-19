d222352046ff95a54cb15e99f373e46a6d92c8b8
denyshorshkov <denys.horshcov@gmail.com>
2025-07-19 10:43:40 +0300

    fashion edit

--- parse_stocks.py
+++ parse_stocks.py
@@ -2,18 +2,16 @@ import yfinance as yf
 import pandas as pd
 import os
 
-# ---------- 1. Налаштування ----------
-
 ticker = 'NVDA'
-start_date = '2022-01-01'  # дата до релізу RTX 40xx
-end_date = '2025-06-01'    # поточна дата або межа дослідження
+start_date = '2022-01-01'  
+end_date = '2025-06-01'    
 
 output_path = "tables"
 os.makedirs(output_path, exist_ok=True)
 
-# ---------- 2. Завантаження історичних цін акцій NVIDIA ----------
+# Завантаження  цін акцій NVIDIA
 
-print(f"📥 Завантаження цін акцій для {ticker}...")
+print(f"Завантаження цін акцій для {ticker}...")
 data = yf.download(ticker, start=start_date, end=end_date, auto_adjust=False, group_by='column')
 
 if isinstance(data.columns, pd.MultiIndex):
@@ -32,10 +30,10 @@ stock_prices_df = data.rename(columns={
     "Volume": "volume"
 })[['ticker', 'date', 'open', 'high', 'low', 'close', 'adj_close', 'volume']]
 
-# ---------- 3. Збереження у CSV для подальшого завантаження в PostgreSQL ----------
+# Збереження у CSV
 
 stock_prices_df.to_csv(os.path.join(output_path, "stock_prices.csv"), index=False)
 
-print("\n✅ Дані успішно збережено у 'tables/stock_prices.csv'.")
+print("\nДані успішно збережено у 'tables/stock_prices.csv'.")
 
 
--- upload.py
+++ upload.py
@@ -15,17 +15,16 @@ db_name = os.getenv("DB_NAME")
 # Create connection string
 engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
 
-# --------- 2. Load CSV files ---------
+# Load CSV files
 stock_prices_df = pd.read_csv("tables/stock_prices.csv")
 rtx_4070_df = pd.read_csv("tables/rtx4070.csv")
 rtx_4080_df = pd.read_csv("tables/rtx4080.csv")
 rtx_4090_df = pd.read_csv("tables/rtx4090.csv")
 
-# --------- 3. Upload to PostgreSQL ---------
-# Replace old table if exists
+# Upload to PostgreSQL 
 stock_prices_df.to_sql("stock_prices", engine, if_exists="replace", index=False)
 rtx_4070_df.to_sql("rtx_4070", engine, if_exists="replace", index=False)
 rtx_4080_df.to_sql("rtx_4080", engine, if_exists="replace", index=False)
 rtx_4090_df.to_sql("rtx_4090", engine, if_exists="replace", index=False)
 
-print("✅ Data successfully uploaded to PostgreSQL.")
+print("Data successfully uploaded to PostgreSQL.")
